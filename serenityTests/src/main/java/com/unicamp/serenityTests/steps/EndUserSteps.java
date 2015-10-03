aa74bc3196009abf9931d5aa91899a7ce91b4b00
pereirfe <ra104922@pluto.lab.ic.unicamp.br>
2015-10-02 22:23:41 -0300

    Montage Thuc M1F2US4

--- serenityTests/src/main/java/com/unicamp/serenityTests/steps/EndUserSteps.java
+++ serenityTests/src/main/java/com/unicamp/serenityTests/steps/EndUserSteps.java
@@ -1,9 +1,9 @@
 package com.unicamp.serenityTests.steps;
 
 import com.unicamp.serenityTests.pages.MainPepPage;
+
 import net.thucydides.core.annotations.Step;
 import net.thucydides.core.steps.ScenarioSteps;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
@@ -23,4 +23,37 @@ public class EndUserSteps extends ScenarioSteps {
     public void is_the_main_page() {
         dictionaryPage.open();
     }
+
+	public void isRecepcionist(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void isDoctor(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void isOpen(String nome) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void selectField(String string, String dados_Clinicos) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void selectProfile(String string, String nome) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void showDemographicPage(String nome, String telefone,
+			String generoAdm, String horNasc, String end, String estado_Civil,
+			String race, String etnia) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
