427f4a872580342598c855088ee0907519474dbe
italomg <italogmoraes@gmail.com>
2015-10-09 23:33:18 -0300

    Criando arquivo de page object para inserir paciente

---
+++ serenityTests/src/main/java/com/unicamp/serenityTests/pages/InsertPatientPage.java
@@ -0,0 +1,12 @@
+package com.unicamp.serenityTests.pages;
+
+import net.serenitybdd.core.pages.PageObject;
+import ch.lambdaj.function.convert.Converter;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class InsertPatientPage extends PageObject {
+
+}
