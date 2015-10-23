c61009c5c10577efc3e313f078e09909e8a94a35
Francesco Perrotti-Garcia <fpg1503@gmail.com>
2015-10-23 21:24:27 -0200

    Create methods to Start SOAPE Steps

--- serenityTests/src/main/java/com/unicamp/serenityTests/steps/StartSOAPESteps.java
+++ serenityTests/src/main/java/com/unicamp/serenityTests/steps/StartSOAPESteps.java
@@ -18,87 +18,22 @@ public class StartSOAPESteps extends ScenarioSteps {
 	SOAPEPage SOAPEPg;
 
 	@Step
-	public void is_on_the_new_patient_page() {
-		insertPatientPage.open();
+	public void isOnTheSearchPage() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Step
+	public void clickModeStartSOAPEForUser(String user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Step
+	public void shouldSeeSOAPEModeForUser(String user) {
+		// TODO Auto-generated method stub
+		
 	}
 	
-	@Step
-	public void insertName(String name) {
-		insertPatientPage.typeName(name);
-	}
-
-	@Step
-	public void insertPhoneNumber(String phoneNumber) {
-		insertPatientPage.typePhone(phoneNumber);
-	}
-
-	@Step
-	public void selectGender(String gender) {
-		insertPatientPage.chooseGender(gender);
-	}
-
-	@Step
-	public void insertBirthDate(String birthDate) {
-		insertPatientPage.typeBirthDate(birthDate);
-	}
-
-	@Step
-	public void insertAddress(String address) {
-		insertPatientPage.typeAddress(address);
-	}
-
-	@Step
-	public void selectMaritalState(String maritalState) {
-		insertPatientPage.chooseMaritalStatus(maritalState);
-	}
-
-	@Step
-	public void selectRace(String race) {
-		insertPatientPage.chooseBreed(race);
-	}
-
-	@Step
-	public void selectEthnicity(String ethnicity) {
-		insertPatientPage.chooseEthnicity(ethnicity);
-	}
-
-	@Step
-	public void saveForm() {
-		insertPatientPage.clickOnSave();
-	}
-
-	@Step
-	public void cancelForm() {
-		insertPatientPage.clickOnCancel();
-	}
-
-	@Step
-	public void fillForm(String name, String phone, String gender,String birth,
-			String address,String maritalstatus,String breed,String ethinicity) {
-
-		insertName(name);
-		insertPhoneNumber(phone);
-		selectGender(gender);
-		insertBirthDate(birth);
-		insertAddress(address);
-		selectMaritalState(maritalstatus);
-		selectRace(breed);
-		selectEthnicity(ethinicity);
-	}
-
-	@Step
-	public void insert_data(String name, String phoneNumber, String gender,
-			String birthDate, String address, String maritalState, String race,
-			String ethnicity) {
-
-		fillForm(name, phoneNumber, gender, birthDate, address, maritalState,
-				race, ethnicity);
-		saveForm();
-	}
-	
-	@Step
-	public void shoudlSeeMessage(String message) {
-		assertThat("Message was on screen", insertPatientPage.getMessage().equals(message));
-	}
 
 }
